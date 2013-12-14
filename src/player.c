5c4c0b7bfd5688bae8aae441bc82a21e2e5fbe5e
Charlie Hadden <charlie@hadden.me>
2013-12-14 02:28:01 +0000

    Player now based on proper x/y positions and variable sizing

--- src/player.c
+++ src/player.c
@@ -1,7 +1,13 @@
 #include "player.h"
 
+static int x_ = 0, y_ = 0;
+static int size_ = 0;
+
 void
 player_init(void) {
+	x_ = 300;
+	y_ = 600;
+	size_ = 50;
 }
 
 void
@@ -13,14 +19,14 @@ player_draw(void) {
 	glColor3f(0.8f, 0.2f, 0.2f);
 
 	glPushMatrix();
-	glTranslatef(200.0f, 400.0f, 0.0f);
+	glTranslatef(x_, y_, 0.0f);
 
 	glBegin(GL_QUADS);
 
-	glVertex2f(-50.0f, -50.0f);
-	glVertex2f( 50.0f, -50.0f);
-	glVertex2f( 50.0f,  50.0f);
-	glVertex2f(-50.0f,  50.0f);
+	glVertex2f(-size_ / 2, -size_ / 2);
+	glVertex2f( size_ / 2, -size_ / 2);
+	glVertex2f( size_ / 2,  size_ / 2);
+	glVertex2f(-size_ / 2,  size_ / 2);
 
 	glEnd();
 	glPopMatrix();
